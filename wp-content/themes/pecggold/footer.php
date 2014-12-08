898fe262d3fb3c9402a4db78ef5c8b8b682948d8
q <qpham415@gmail.com>
2014-12-08 11:59:37 -0800

    Update footer and style sidebar

--- wp-content/themes/pecggold/footer.php
+++ wp-content/themes/pecggold/footer.php
@@ -5,6 +5,14 @@
     <p>PECG Golden Gate</p>
     <p style="font-size: 20px">Engineering California's Future</p>
   </div>
+  <div class="socialMedia">
+    <div class="socialItem">
+      <a href=""><img src="wp-content/themes/pecggold/images/twitter.png" height="35px" width="35px"></a>
+    </div>
+    <div class="socialItem">
+      <a href=""><img src="wp-content/themes/pecggold/images/youtube.png" height="35px" width="35px"></div></a>
+    </div>
+  </div>
   <div id="disclaimer"></div>
 </footer>
 </div>
