8c1348e80b3d733e98c9dd4de079ad4f8ee3e578
q <qpham415@gmail.com>
2014-12-11 14:18:58 -0800

    style single page

--- wp-content/themes/pecggold/entry-footer.php
+++ wp-content/themes/pecggold/entry-footer.php
@@ -1,7 +1,9 @@
-<footer class="entry-footer">
-<span class="cat-links"><?php _e( 'Categories: ', 'blankslate' ); ?><?php the_category( ', ' ); ?></span>
-<span class="tag-links"><?php the_tags(); ?></span>
-<?php if ( comments_open() ) { 
-echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'blankslate' ) ) . '</a></span>';
-} ?>
-</footer> 
+<div class="entry-footer">
+  <div class="entry-utility">
+    <span class="cat-links"><span class="entry-utility-prep entry-utility-prep-cat-links"><?php _e( 'Posted in ', 'hbd-theme' ); ?></span><?php echo get_the_category_list(', '); ?></span>
+    <span class="meta-sep"> | </span>
+    <?php the_tags( '<span class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">' . __('Tagged ', 'hbd-theme' ) . '</span>', ", ", "</span>\n\t\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
+    <span class="postDate"><?php $my_date = the_date('', 'Posted on ', '', FALSE); echo $my_date; ?></span>
+    <?php edit_post_link( __( 'Edit', 'hbd-theme' ), "<span class=\"meta-sep\">|</span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t\n" ) ?>
+  </div><!-- #entry-utility -->
+</div>
