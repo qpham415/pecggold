8d6ada0d28609506764b75194fa439615ac9b3f4
q <qpham415@gmail.com>
2014-12-10 17:03:15 -0800

    fixed default page template + restyle fonts

--- wp-content/themes/pecggold/default-page.php
+++ wp-content/themes/pecggold/default-page.php
@@ -1,19 +1,14 @@
 <?php
 /*
-Template Name: Default Template
+Template Name: Standard Template
 */
 get_header();
 ?>
 <div class="homePage">
   <?php the_post(); ?>
 
-
-  <div class="pageHeader">
-    <?php the_block('homeHeader') ?>
-  </div>
   <div class="pageWrapper" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    <div class="clear"></div>
     <div class="pageContent">
       <?php the_content(); ?>
     </div>
