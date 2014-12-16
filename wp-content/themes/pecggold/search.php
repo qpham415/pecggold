3e82cbadc87f951aee823adecfbe2f28fd6e3c7a
q <qpham415@gmail.com>
2014-12-15 22:52:13 -0800

    restyle search page

--- wp-content/themes/pecggold/search.php
+++ wp-content/themes/pecggold/search.php
@@ -1,24 +1,28 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<?php if ( have_posts() ) : ?>
-<header class="header">
-<h1 class="entry-title"><?php printf( __( 'Search Results for: %s', 'blankslate' ), get_search_query() ); ?></h1>
-</header>
-<?php while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-<?php else : ?>
-<article id="post-0" class="post no-results not-found">
-<header class="header">
-<h2 class="entry-title"><?php _e( 'Nothing Found', 'blankslate' ); ?></h2>
-</header>
-<section class="entry-content">
-<p><?php _e( 'Sorry, nothing matched your search. Please try again.', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
-</section>
-</article>
-<?php endif; ?>
-</section>
+<div id="pageContainer" role="main">
+  <div id="blogWrap">
+    <div id="blogContent" class="searchContent">
+      <?php if ( have_posts() ) : ?>
+      <div class="sortedTitle entry-title"><?php printf( __( 'Search Results For: %s', 'blankslate' ), get_search_query() ); ?></div>
+      <div class="clear"></div>
+      <?php while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part( 'entry' ); ?>
+      <?php endwhile; ?>
+      <?php get_template_part( 'nav', 'below' ); ?>
+    </div>
+    <div class="blogSidebar">
+      <?php get_sidebar(); ?>
+    </div>
+    <?php else : ?>
+      <div class="sortedTitle entry-title"><?php printf( __( 'Search Results For: %s', 'blankslate' ), get_search_query() ); ?></div>
+      <div class="clear"></div>
+      <div class="centerTitle entry-title"><?php _e( 'Sorry, nothing matched your search. Please try again.', 'blankslate' ); ?></div>
+    </div>
+    <div class="blogSidebar">
+      <?php get_sidebar(); ?>
+    </div>
+    <?php endif; ?>
+  </div>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
