4c589f09ec78b1fa2007d6f44ed09291085925bf
q <qpham415@gmail.com>
2014-12-15 21:29:30 -0800

    update category page

--- wp-content/themes/pecggold/category.php
+++ wp-content/themes/pecggold/category.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
 <div id="pageContainer" role="main">
-  <div id="categoryContent">
-    <header class="header">
-      <h1 class="entry-title"><?php _e( 'Category Archives: ', 'blankslate' ); ?><?php single_cat_title(); ?></h1>
+  <div id="blogWrap">
+    <div id="blogContent">
+      <div class="sortedTitle entry-title"><?php _e( 'Category Archives: ', 'blankslate' ); ?><?php single_cat_title(); ?></div>
+      <div class="clear"></div>
       <?php if ( '' != category_description() ) echo apply_filters( 'archive_meta', '<div class="archive-meta">' . category_description() . '</div>' ); ?>
-    </header>
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <?php get_template_part( 'entry' ); ?>
-    <?php endwhile; endif; ?>
-    <?php get_template_part( 'nav', 'below' ); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part( 'entry' ); ?>
+      <?php endwhile; endif; ?>
+      <?php get_template_part( 'nav', 'below' ); ?>
+    </div>
+    <div class="blogSidebar">
+      <?php get_sidebar(); ?>
+    </div>
   </div>
 </div>
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
