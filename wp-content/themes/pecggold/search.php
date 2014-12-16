b094d709cad92dd4906d6da2b93b058105855309
q <qpham415@gmail.com>
2014-12-15 22:56:24 -0800

    search page restyle

--- wp-content/themes/pecggold/search.php
+++ wp-content/themes/pecggold/search.php
@@ -1,26 +1,27 @@
 <?php get_header(); ?>
 <div id="pageContainer" role="main">
   <div id="blogWrap">
-    <div id="blogContent" class="searchContent">
-      <?php if ( have_posts() ) : ?>
-      <div class="sortedTitle entry-title"><?php printf( __( 'Search Results For: %s', 'blankslate' ), get_search_query() ); ?></div>
-      <div class="clear"></div>
-      <?php while ( have_posts() ) : the_post(); ?>
-      <?php get_template_part( 'entry' ); ?>
-      <?php endwhile; ?>
-      <?php get_template_part( 'nav', 'below' ); ?>
-    </div>
-    <div class="blogSidebar">
-      <?php get_sidebar(); ?>
-    </div>
+    <?php if ( have_posts() ) : ?>
+      <div id="blogContent">
+        <div class="sortedTitle entry-title"><?php printf( __( 'Search Results For: %s', 'blankslate' ), get_search_query() ); ?></div>
+        <div class="clear"></div>
+        <?php while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part( 'entry' ); ?>
+        <?php endwhile; ?>
+        <?php get_template_part( 'nav', 'below' ); ?>
+      </div>
+      <div class="blogSidebar">
+        <?php get_sidebar(); ?>
+      </div>
     <?php else : ?>
-      <div class="sortedTitle entry-title"><?php printf( __( 'Search Results For: %s', 'blankslate' ), get_search_query() ); ?></div>
-      <div class="clear"></div>
-      <div class="centerTitle entry-title"><?php _e( 'Sorry, nothing matched your search. Please try again.', 'blankslate' ); ?></div>
-    </div>
-    <div class="blogSidebar">
-      <?php get_sidebar(); ?>
-    </div>
+      <div id="blogContent" class="searchContent">
+        <div class="sortedTitle entry-title"><?php printf( __( 'Search Results For: %s', 'blankslate' ), get_search_query() ); ?></div>
+        <div class="clear"></div>
+        <div class="centerTitle entry-title"><?php _e( 'Sorry, nothing matched your search. Please try again.', 'blankslate' ); ?></div>
+      </div>
+      <div class="blogSidebar">
+        <?php get_sidebar(); ?>
+      </div>
     <?php endif; ?>
   </div>
 </div>
